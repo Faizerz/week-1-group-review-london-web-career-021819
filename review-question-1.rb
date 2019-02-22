c51302e9cef432a7807b74a043cd155b8c739b60
github username <github email address>
2019-02-22 10:36:45 +0000

    Done

--- review-question-1.rb
+++ review-question-1.rb
@@ -1,5 +1,5 @@
 ## QUESTION 1
-
+require 'pry'
 pokemon = [
   {
     "id": 1,
@@ -62,8 +62,64 @@ pokemon = [
 
 
 # How would you get the url for Bulbasaur's ability?
+def bulbasaur_ability(name, pokemon)
+  pokemon.each do |pokemon_hash|
+    if pokemon_hash[:name] == name
+      pokemon_hash[:abilities].each do |ability_hash|
+        return ability_hash[:ability][:url]
+      end
+    end
+  end
+end
+
+# puts bulbasaur_ability('bulbasaur', pokemon)
+
+
 # How would you return the first pokemon with base experience over 40?
+def find_fourty(pokemon)
+  pokemon.find do |pokemon|
+    pokemon[:base_experience] > 40
+  end
+end
+#
+# puts find_fourty(pokemon)
+
+
+
+
 # How would you return ALL OF THE pokemon with base experience over 40? (Gotta catch em all)
+def find_fourty(pokemon)
+  pokemon.select do |pokemon|
+    pokemon[:base_experience] > 40
+  end
+end
+
+# puts find_fourty(pokemon)
+
+
+
 # How would you return an array of all of the pokemon's names?
+def poke_names(pokemon)
+  pokemon.map do |poke|
+    poke[:name]
+  end
+end
+
+#
+# puts poke_names(pokemon)
+
+
+
+
 # How would you determine whether or not the pokemon array contained any pokemon with a weight greater than 60?
+def poke_weight(pokemon)
+  pokemon.each do |pokemon_hash|
+    return pokemon_hash[:weight]>60 ? true : false
+  end
+end
+#
+# puts poke_weight(pokemon)
+
+
+
 #  whatever method you use should return true if there are any such pokemon, false if not.
--- review-question-3.rb
+++ review-question-3.rb
@@ -1,13 +1,69 @@
 # begin to build a simple program that models Instagram
 # you should have a User class, a Photo class and a comment class
+require "pry"
 
+class User
+
+  attr_reader :name
+  attr_accessor :user
+
+  def initialize(name)
+    @name = name
+  end
+
+  def photos
+    Photo.all.select do |photo|
+      photo.user == self
+    end
+  end
+end
+
+class Photo
+  attr_accessor :user
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def comments
+    Comment.all.select do |comment|
+      comment.photo == self
+    end
+  end
+
+  def make_comment(string)
+    comment = Comment.new(string)
+    comment.photo = self
+  end
+
+  def self.all
+    @@all
+  end
+end
+
+class Comment
+  @@all = []
+  attr_reader :comment
+  attr_accessor :photo
+  def initialize(comment)
+    @comment = comment
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
 
 sandwich_photo = Photo.new
 sophie = User.new("Sophie")
+
 sandwich_photo.user = sophie
 sandwich_photo.user.name
 # => "Sophie"
-user.photos
+
+sophie.photos
 # => [#<Photo:0x00007fae2880b370>]
 
 
@@ -15,8 +71,10 @@ sandwich_photo.comments
 # => []
 
 sandwich_photo.make_comment("this is such a beautiful photo of your lunch!! I love photos of other people's lunch")
+sandwich_photo.make_comment("this is such a NAAof other people's lunch")
 sandwich_photo.comments
 # => [#<Comment:0x00007fae28043700>]
 
 Comment.all
 #=> [#<Comment:0x00007fae28043700>]
+pry
